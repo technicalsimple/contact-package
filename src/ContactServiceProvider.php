23873ff6ae8a6be411c3c60fcecfb7115d608e63
technicalsimpe <technicalsimplet2s@gmail.com>
2021-12-12 00:34:10 +0530

    update readme

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -7,7 +7,7 @@ class ContactServiceProvider extends  ServiceProvider
 {
     public function register()
     {
-      //
+        $this->app->make('Http/Controllers/ContactController.php');
     }
 
     public function boot()
@@ -15,7 +15,7 @@ class ContactServiceProvider extends  ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadViewsFrom(__DIR__.'/views','contact');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        $this->mergeConfigFrom(__DIR__.'/config/contact.php','contact');
+        $this->mergeConfigFrom(__DIR__.'/config/contact.php','contact');        
         $this->publishes([
             __DIR__.'/config/contact.php' => config_path('contact.php'),
             __DIR__.'/views' => resource_path('views/vendor/contact'),
